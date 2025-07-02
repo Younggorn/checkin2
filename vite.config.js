e1ab786b0a92ead91907d033b3c8310eeafda23a
Younggorn <phanloloc1@gmail.com>
2025-07-02 16:07:21 +0700

    fdfdf

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/checkin2/',
   plugins: [react(), tailwindcss()],
-  base: "/checkin2/",
+  
 })
